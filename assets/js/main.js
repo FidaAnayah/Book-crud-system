a2cece02a4c93d355b0555b3934b24a701ea7b78
FidaAnayah <eng.fida.hamad@gmail.com>
2022-05-29 16:19:30 +0300

    validation and sweetalert

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,18 +26,19 @@ addBtn.onclick = function(){
     clearForm()
 }
 function addBook(){
-var book = {
+    var book = {
     title:btitle.value,
     auther: bauther.value,
     publisher: bpub.value,
     genre: bgenre.value,
     price:bprice.value
 };
+
 books.push(book);
 localStorage.setItem("booksList",JSON.stringify(books));
 }
 function displayData(){
-    var result = "";
+        var result = "";
 for (var i = 0; i<books.length;i++){
     result+=
      `<tr>
@@ -85,6 +86,7 @@ function  getData(index){
     currentIndex = index;
 }
 function  updateData(){
+    
     var book = {
         title:btitle.value,
         auther: bauther.value,
@@ -99,6 +101,13 @@ function  updateData(){
     books[currentIndex].price = book.price;
     localStorage.setItem("booksList",JSON.stringify(books));
     addBtn.innerHTML ="Add Book";
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
 }
 function clearForm(){
     for(var i = 0 ; i< inputs.length;i++){
@@ -106,19 +115,55 @@ function clearForm(){
     }
 }
 function deleteBook(index){
-    books.splice(index,1);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            books.splice(index,1);
     localStorage.setItem("booksList",JSON.stringify(books));
     displayData();
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+    
 }
 deleteAll.onclick = function(){
-    localStorage.removeItem("booksList");
-    books=[];
-    data.innerHTML = "";
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("booksList");
+            books=[];
+            data.innerHTML = "";
+          Swal.fire(
+            'Deleted!',
+            'All files has been deleted.',
+            'success'
+          )
+        }
+      })
+   
 }
 
 title.onkeyup = function(){
-    var titlePattern = /^[A-Z][a-z0-9]{2,10}$/;
-   // console.log(titlePattern.test(title.value));
+    var titlePattern = /^[A-Z][a-z0-9]{2,20}$/;
+    //console.log(titlePattern.test(title.value));
    if(titlePattern.test(title.value)){
     addBtn.removeAttribute("disabled");
     title.classList.add('is-valid');
@@ -127,9 +172,81 @@ title.onkeyup = function(){
        
    }
    else{
-       addBtn.disabled = true;
+      addBtn.disabled = true;
        title.classList.add('is-invalid');
        title.classList.remove('is-valid');
        titleAlert.classList.remove('d-none')
    }
+  
+   }
+auther.onkeyup = function(){
+    var autherPattern = /^[A-Z][a-z]{2,20}$/;
+     if(autherPattern.test(auther.value)){
+
+        addBtn.removeAttribute("disabled");
+        auther.classList.add('is-valid');
+        auther.classList.remove('is-invalid');
+        autherAlert.classList.add('d-none');
+           
+       }
+       else{
+           addBtn.disabled = true;
+           auther.classList.add('is-invalid');
+           auther.classList.remove('is-valid');
+           autherAlert.classList.remove('d-none')
+       }
 }
+publisher.onkeyup = function(){
+    var publisherPattern = /^[A-Z][a-z]{2,20}$/;
+     if(publisherPattern.test(publisher.value)){
+
+        addBtn.removeAttribute("disabled");
+        publisher.classList.add('is-valid');
+        publisher.classList.remove('is-invalid');
+        publisherAlert.classList.add('d-none');
+           
+       }
+       else{
+           addBtn.disabled = true;
+           publisher.classList.add('is-invalid');
+           publisher.classList.remove('is-valid');
+           publisherAlert.classList.remove('d-none')
+       }
+}
+genre.onkeyup = function(){
+    var genrePattern = /^[a-zA-Z ]/;
+     if(genrePattern.test(genre.value) != ""){
+      
+        addBtn.removeAttribute("disabled");
+        genre.classList.add('is-valid');
+        genre.classList.remove('is-invalid');
+        genreAlert.classList.add('d-none');
+           
+       }
+       else{
+           addBtn.disabled = true;
+           genre.classList.add('is-invalid');
+           genre.classList.remove('is-valid');
+           genreAlert.classList.remove('d-none')
+       }
+}
+price.onkeyup = function(){
+    var pricePattern = /^\d{0,8}[.]?\d{1,4}$/;
+     if(pricePattern.test(price.value)){
+
+        addBtn.removeAttribute("disabled");
+        price.classList.add('is-valid');
+        price.classList.remove('is-invalid');
+        priceAlert.classList.add('d-none');
+           
+       }
+       else{
+           addBtn.disabled = true;
+           price.classList.add('is-invalid');
+           price.classList.remove('is-valid');
+           priceAlert.classList.remove('d-none')
+       }
+}
+
+
+
